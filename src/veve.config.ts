1c99edbb32b3d9895b4751c83183e36cea4a0a8e
Solo Dev is Good <151962661+solo-fox@users.noreply.github.com>
2024-10-31 15:32:40 +0000

    Fix: Typescript issue

--- src/veve.config.ts
+++ src/veve.config.ts
@@ -2,7 +2,8 @@ import type { Veve } from "./config/Config";
 
 const config: Veve = {
   pattern: ["**/*.test.ts", "**/*.test.js", "**/*.spec.ts", "**/*.spec.js"],
-  exclude: ["**/dist/**"]
+  exclude: ["**/dist/**"],
+  reporters: ["spec"]
 };
 
 export default config;
